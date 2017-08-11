13e5442381f5f51252d3c7ab07e00e688bd96dfa
AlexocDS <ajandro43@hotmail.com>
2017-08-11 18:55:59 -0500

    documentación
    Encabezado de documentación

--- Proyecto/EddyBurguerR/application/models/Contactom.php
+++ Proyecto/EddyBurguerR/application/models/Contactom.php
@@ -1,4 +1,20 @@
 <?php
+/**
+*Clase Contactom
+*Esta clase extiende de CI_Model, y funciona como modelo para las vistas back-end pertenecientes a la gestión
+*de comentarios.
+*/
+
+/**
+*@category EddyBurguer
+*@package EddyBurguerR
+*@subpackage controllers
+*@copyright Derechos reservados® Soft-pack
+*@version 0.1
+*@link https://github.com/MigAnge/SEB_001/blob/master/Proyecto/EddyBurguerR/application/models/Contactom.php
+*@author Alejandro Onofre Cornejo
+*@since 0.1
+*/
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Contactom extends CI_Model {
